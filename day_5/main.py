29ae4635e958586d923ed8f063ed9a0a73987fb9
Skyler Kimura <skyler8@hawaii.edu>
2025-07-25 23:35:47 -1000

    completed day 5

--- day_5/main.py
+++ day_5/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import cmp_to_key
 
 
 def validate_pages(pages_arr: list[str]) -> tuple[list[list[str]], list[list[str]]]:
@@ -8,7 +9,7 @@ def validate_pages(pages_arr: list[str]) -> tuple[list[list[str]], list[list[str
         page_nums = page.split(",")
         valid = True
         for x in range(1, len(page_nums)):
-            if page_nums[x] not in rule_dict[page_nums[x-1]]:
+            if page_nums[x] not in rule_dict[page_nums[x - 1]]:
                 invalid_pages.append(page_nums)
                 valid = False
                 break
@@ -17,37 +18,34 @@ def validate_pages(pages_arr: list[str]) -> tuple[list[list[str]], list[list[str
 
     return (valid_pages, invalid_pages)
 
+
+def compare_pages(a, b):
+    if b in rule_dict[a]:
+        return -1
+    elif a in rule_dict[b]:
+        return 1
+    else:
+        return 0
+
+
 def correct_page(page_arr: list[str]) -> list[str]:
-    correct_page = page_arr.copy()
-    x = 1
-    while x < len(correct_page):
-        y = 0
-        while x + y < len(correct_page) and correct_page[x + y] not in rule_dict.get(correct_page[x + y], []):
-            y += 1
+    return sorted(page_arr, key=cmp_to_key(compare_pages))
 
-        if x + y < len(correct_page):
-            item = correct_page.pop(x + y)
-            correct_page.insert(x + y, item)
-        x += 1
-
-
-    return correct_page 
 
 if __name__ == "__main__":
     input = ""
-    with open("example.txt", "r") as f:
+    with open("input.txt", "r") as f:
         input = f.read()
 
     rules, pages = input.split("\n\n")
 
     rule_dict = defaultdict(list)
-    
+
     rules_arr = rules.split()
 
     for rule in rules_arr:
         rule1, rule2 = rule.split("|")
         rule_dict[rule1].append(rule2)
-
 
     pages_arr = pages.split()
     valid_pages, invalid_pages = validate_pages(pages_arr)
@@ -57,11 +55,9 @@ if __name__ == "__main__":
     for valid_page in valid_pages:
         sum_1 += int(valid_page[len(valid_page) // 2])
 
-
     for page in invalid_pages:
         corrected_pages = correct_page(page)
-        print(corrected_pages)
-        sum_2 += int(page[len(page) // 2])
+        sum_2 += int(corrected_pages[len(corrected_pages) // 2])
 
     print(sum_1)
     print(sum_2)
